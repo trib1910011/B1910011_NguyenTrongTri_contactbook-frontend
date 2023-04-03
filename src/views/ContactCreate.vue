ef22656bb723930ef4a13b0e69f79c565240c6f4
trib1910011 <trib1910011@student.ctu.edu.vn>
2023-04-03 21:24:09 +0700

    Tao trang them moi mot lien he

---
+++ src/views/ContactCreate.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1 class="text-center"> Thêm Liên hệ</h1>
+    <ContactForm @submit:contact="createContact" :contact="contact" class="p-5"></ContactForm>
+    <p>{{ message }}</p>
+</template>
+
+<script>
+import ContactForm from '../components/ContactForm.vue';
+import contactService from '../services/contact.service';
+export default {
+    components: {
+        ContactForm
+    },
+
+    data() {
+        return {
+            contact: {},
+            message: ""
+        }
+    },
+    methods: {
+        async createContact(data) {
+            try {
+                await contactService.create(data)
+                this.message = "Liên hệ được tạo"
+            }
+            catch (error) {
+                console.log(error)
+            }
+        }
+    }
+}
+</script>
+
